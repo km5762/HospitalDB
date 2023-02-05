6f4661294025cf468b511827a50e452959062544
km5762 <km5762@DESKTOP-587P2TU>
2023-02-05 15:26:41 -0500

    Merge

--- HospitalDB.sql
+++ HospitalDB.sql
@@ -3,84 +3,55 @@ CREATE TABLE doctor (
     gender        VARCHAR2(20),
     specialty     VARCHAR2(20),
     graduatedfrom VARCHAR2(20),
-    CONSTRAINT doctor_pk PRIMARY KEY (employeeid),
-    CONSTRAINT doctor_id_fk FOREIGN KEY (employeeid) REFERENCES employee(id)
+    CONSTRAINT doctor_pk PRIMARY KEY ( employeeid ),
+    CONSTRAINT doctor_id_fk FOREIGN KEY ( employeeid )
+        REFERENCES employee ( id )
 );
 
 CREATE TABLE equipmenttechnician (
     employeeid INTEGER,
-    CONSTRAINT equipmenttechnician_pk PRIMARY KEY (employeeid),
-    CONSTRAINT equipmenttechnician_id_fk FOREIGN KEY (employeeid) REFERENCES employee(id)
+    CONSTRAINT equipmenttechnician_pk PRIMARY KEY ( employeeid ),
+    CONSTRAINT equipmenttechnician_id_fk FOREIGN KEY ( employeeid )
+        REFERENCES employee ( id )
 );
 
 CREATE TABLE canrepairequipment (
     employeeid    INTEGER,
     equipmenttype VARCHAR2(20),
-    CONSTRAINT canrepairequipment_pk PRIMARY KEY (employeeid, equipmenttype)
-);
-
-CREATE TABLE equipmenttype (
-    eid            INTEGER,
-    edesc     VARCHAR2(20),
-    emodel         VARCHAR2(20),
-    instructions  VARCHAR2(20),
-    numberofunits INTEGER
+    CONSTRAINT canrepairequipment_pk PRIMARY KEY ( employeeid,
+                                                   equipmenttype )
 );
 
 CREATE TABLE equipment (
-    serialnum        INTEGER,
-    typeid         INTEGER,
-    purchaseyear   INTEGER,
-    lastinspection DATE,
-    roomnum        INTEGER
+    serialnum       INTEGER,
+    equipmenttypeid INTEGER,
+    purchaseyear    INTEGER,
+    lastinspection  DATE,
+    roomnum         INTEGER
 );
 
 CREATE TABLE employee (
-    id            NUMBER PRIMARY KEY,
+    employeeid    NUMBER PRIMARY KEY,
     fname         VARCHAR2(40),
     lname         VARCHAR2(40),
     salary        INTEGER,
     jobtitle      VARCHAR(40),
     officenum     INTEGER,
     emprank       VARCHAR2(40),
-    supervisorid  INTERGER,
+    supervisorid  interger,
     addressstreet VARCHAR2(40),
     addresscity   VARCHAR2(40),
     addresszip    INTEGER,
-    Constraint employee_supervisorid_fk FOREIGN KEY ( supervisorid )
+    CONSTRAINT employee_supervisorid_fk FOREIGN KEY ( supervisorid )
         REFERENCES employee ( id )
 )
 
-CREATE TABLE doctor (
-    employeeid    INTEGER,
-    gender        VARCHAR2,
-    specialty     VARCHAR2,
-    graduatedfrom VARCHAR2
-);
-
-CREATE TABLE equipmenttechnician (
-    employeeid INTEGER
-);
-
-CREATE TABLE canrepairequipment (
-    employeeid    INTEGER,
-    equipmenttype VARCHAR2
-);
-
 CREATE TABLE equipmenttype (
-    id            INTEGER,
-    desc          VARCHAR2,
-    model         VARCHAR2,
-    instructions  VARCHAR2,
-    numberofunits INTEGER
-);
-
-CREATE TABLE equipment (
-    serialnum      INTEGER,
-    typeid         INTEGER,
-    purchaseyear   INTEGER,
-    lastinspection DATE,
-    roomnum        INTEGER
+    equipmenttypeid INTEGER,
+    equipmentdesc   VARCHAR2,
+    equipmentmodel  VARCHAR2,
+    instructions    VARCHAR2,
+    numberofunits   INTEGER
 );
 
 CREATE TABLE room (
@@ -110,24 +81,25 @@ CREATE TABLE roomaccess (
 )
 
 CREATE TABLE patient (
-    ssn number,
-    firstname Varchar2(40),
-    lastname Varchar2(40),
-    address Varchar2(40),
-    telnum integer,
-    Constraint pk_patient primary key (ssn)
+    ssn       NUMBER,
+    firstname VARCHAR2(40),
+    lastname  VARCHAR2(40),
+    address   VARCHAR2(40),
+    telnum    INTEGER,
+    CONSTRAINT pk_patient PRIMARY KEY ( ssn )
 )
 
 CREATE TABLE admission (
-    anum integer,
-    admissiondate date,
-    leavedate date,
-    totalpayment real,
-    insurancepayment real,
-    patient_ssn integer,
-    futurevisit date,
-    foreign key (patient_ssn) references patient (ssn),
-    Constraint pk_admission primary key (anum)
+    anum             INTEGER,
+    admissiondate    DATE,
+    leavedate        DATE,
+    totalpayment     REAL,
+    insurancepayment REAL,
+    patient_ssn      INTEGER,
+    futurevisit      DATE,
+    FOREIGN KEY ( patient_ssn )
+        REFERENCES patient ( ssn ),
+    CONSTRAINT pk_admission PRIMARY KEY ( anum )
 )
 
 CREATE TABLE examine (
