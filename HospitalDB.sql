c8c942399cd0b5ffc26deabc39bb6d61c8ba699f
km5762 <km5762@DESKTOP-587P2TU>
2023-02-05 13:54:28 -0500

    Defining tables

--- HospitalDB.sql
+++ HospitalDB.sql
@@ -1,1 +1,6 @@
-help index;
+CREATE TABLE Doctor(EmployeeID INTEGER, gender VARCHAR2, specialty VARCHAR2, GraduatedFrom VARCHAR2);
+CREATE TABLE EquipmentTechnician(EmployeeID INTEGER);
+CREATE TABLE CanRepairEquipment(EmployeeID INTEGER, EquipmentType VARCHAR2);
+CREATE TABLE EquipmentType(ID INTEGER, Desc VARCHAR2, model VARCHAR2, instructions VARCHAR2, NumberOfUnits INTEGER);
+CREATE TABLE Equipment (Serial# INTEGER, TypeID INTEGER, PurchaseYear INTEGER, LastInspection DATE, roomNum INTEGER);
+
