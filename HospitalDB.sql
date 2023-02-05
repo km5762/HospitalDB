3e02658bb2ede0b0d3ce374593ef263b240b7cf3
km5762 <km5762@DESKTOP-587P2TU>
2023-02-05 16:48:02 -0500

    Finish adding constraints to all tables, compiles

--- HospitalDB.sql
+++ HospitalDB.sql
@@ -1,3 +1,20 @@
+CREATE TABLE employee (
+    employeeid    INTEGER,
+    firstname     VARCHAR2(40),
+    lastname      VARCHAR2(40),
+    salary        INTEGER,
+    jobtitle      VARCHAR(40),
+    officenum     INTEGER,
+    employeerank  VARCHAR2(40),
+    supervisorid  INTEGER,
+    addressstreet VARCHAR2(40),
+    addresscity   VARCHAR2(40),
+    addresszip    INTEGER,
+    CONSTRAINT employee_supervisorid_fk FOREIGN KEY ( supervisorid )
+        REFERENCES employee ( employeeid ),
+    CONSTRAINT employee_pk PRIMARY KEY ( employeeid )
+);
+
 CREATE TABLE doctor (
     employeeid    INTEGER,
     gender        VARCHAR2(20),
@@ -11,100 +28,91 @@ CREATE TABLE doctor (
 CREATE TABLE equipmenttechnician (
     employeeid INTEGER,
     CONSTRAINT equipmenttechnician_pk PRIMARY KEY ( employeeid ),
-    CONSTRAINT equipmenttechnician_employeeid_fk FOREIGN KEY ( employeeid )
+    CONSTRAINT equiptechnician_employeeid_fk FOREIGN KEY ( employeeid )
         REFERENCES employee ( employeeid )
+);
+
+CREATE TABLE room (
+    roomnum      INTEGER,
+    occupiedflag CHAR(1),
+    CONSTRAINT occupied_check CHECK ( occupiedflag IN ( '1', '0' ) ),
+    CONSTRAINT room_pk PRIMARY KEY ( roomnum )
+);
+
+CREATE TABLE equipmenttype (
+    equipmenttypeid INTEGER,
+    equipmentdesc   VARCHAR2(500),
+    equipmentmodel  VARCHAR2(500),
+    instructions    VARCHAR2(500),
+    numberofunits   INTEGER,
+    CONSTRAINT equipmenttypeid_pk PRIMARY KEY ( equipmenttypeid )
 );
 
 CREATE TABLE canrepairequipment (
     employeeid      INTEGER,
     equipmenttypeid INTEGER,
+    CONSTRAINT canrepairequip_equiptypid_fk FOREIGN KEY ( equipmenttypeid )
+        REFERENCES equipmenttype ( equipmenttypeid ),
+    CONSTRAINT canrepairequip_equiptech_fk FOREIGN KEY ( employeeid )
+        REFERENCES equipmenttechnician ( employeeid ),
     CONSTRAINT canrepairequipment_pk PRIMARY KEY ( employeeid,
                                                    equipmenttypeid )
 );
 
 CREATE TABLE equipment (
-    serialnum       INTEGER,
+    serialnum       VARCHAR2(40),
     equipmenttypeid INTEGER,
     purchaseyear    INTEGER,
     lastinspection  DATE,
     roomnum         INTEGER,
     CONSTRAINT equipment_equipmenttypeid_fk FOREIGN KEY ( equipmenttypeid )
-        REFERENCES equipment ( equipmenttypeid ),
+        REFERENCES equipmenttype ( equipmenttypeid ),
+    CONSTRAINT equipment_roomnum_fk FOREIGN KEY ( roomnum )
+        REFERENCES room ( roomnum ),
     CONSTRAINT equipment_pk PRIMARY KEY ( serialnum )
 );
 
-CREATE TABLE employee (
-    employeeid    NUMBER PRIMARY KEY,
-    fname         VARCHAR2(40),
-    lname         VARCHAR2(40),
-    salary        INTEGER,
-    jobtitle      VARCHAR(40),
-    officenum     INTEGER,
-    emprank       VARCHAR2(40),
-    supervisorid  INTEGER,
-    addressstreet VARCHAR2(40),
-    addresscity   VARCHAR2(40),
-    addresszip    INTEGER,
-    CONSTRAINT employee_supervisorid_fk FOREIGN KEY ( supervisorid )
-        REFERENCES employee ( employeeid )
-)
-
-CREATE TABLE equipmenttype (
-    equipmenttypeid INTEGER,
-    equipmentdesc   VARCHAR2,
-    equipmentmodel  VARCHAR2,
-    instructions    VARCHAR2,
-    numberofunits   INTEGER,
-    CONSTRAINT equipmenttypeid_pk PRIMARY KEY ( equipmenttypeid )
-);
-
-CREATE TABLE room (
-    roomnum      NUMBER PRIMARY KEY,
-    occupiedflag CHAR(1),
-    CONSTRAINT occupied_check CHECK ( occupiedflag IN ( '1', '0' ) )
-)
-
 CREATE TABLE roomservice (
-    roomnum NUMBER,
+    roomnum INTEGER,
     service VARCHAR2(255),
     CONSTRAINT roomservice_roomnum_fk FOREIGN KEY ( roomnum )
         REFERENCES room ( roomnum ),
     CONSTRAINT roomservice_pk PRIMARY KEY ( roomnum,
                                             service )
-)
+);
 
 CREATE TABLE roomaccess (
-    roomnum    NUMBER,
-    employeeid NUMBER,
+    roomnum    INTEGER,
+    employeeid INTEGER,
     CONSTRAINT roomaccess_roomnum_fk FOREIGN KEY ( roomnum )
         REFERENCES room ( roomnum ),
     CONSTRAINT roomaccess_empid_fk FOREIGN KEY ( employeeid )
         REFERENCES employee ( employeeid ),
     CONSTRAINT roomaccess_pk PRIMARY KEY ( roomnum,
                                            employeeid )
-)
-
+);
+--ssn will be "111-222-3333"
 CREATE TABLE patient (
-    ssn       NUMBER,
+    ssn       CHAR(12),
     firstname VARCHAR2(40),
     lastname  VARCHAR2(40),
     address   VARCHAR2(40),
     telnum    INTEGER,
     CONSTRAINT pk_patient PRIMARY KEY ( ssn )
-)
+);
 
 CREATE TABLE admission (
-    anum             INTEGER,
+    admissionnum     INTEGER,
     admissiondate    DATE,
     leavedate        DATE,
     totalpayment     NUMBER,
     insurancepayment NUMBER,
-    patientssn       INTEGER,
+    ssn              CHAR(12),
     futurevisit      DATE,
-    FOREIGN KEY ( patientssn )
+    FOREIGN KEY ( ssn )
         REFERENCES patient ( ssn ),
-    CONSTRAINT admission_pk PRIMARY KEY ( anum )
-)
+    CONSTRAINT admission_pk PRIMARY KEY ( admissionnum )
+);
 
 CREATE TABLE examine (
     employeeid     INTEGER,
@@ -113,10 +121,10 @@ CREATE TABLE examine (
     CONSTRAINT examine_doctorid_fk FOREIGN KEY ( employeeid )
         REFERENCES doctor ( employeeid ),
     CONSTRAINT examine_admissionnum_fk FOREIGN KEY ( admissionnum )
-        REFERENCES admission ( anum ),
+        REFERENCES admission ( admissionnum ),
     CONSTRAINT examine_pk PRIMARY KEY ( employeeid,
                                         admissionnum )
-)
+);
 
 CREATE TABLE stayin (
     admissionnum INTEGER,
@@ -126,8 +134,8 @@ CREATE TABLE stayin (
     CONSTRAINT fk_roomnum FOREIGN KEY ( roomnum )
         REFERENCES room ( roomnum ),
     CONSTRAINT stayin_admissionnum_fk FOREIGN KEY ( admissionnum )
-        REFERENCES admission ( anum ),
+        REFERENCES admission ( admissionnum ),
     CONSTRAINT stayin_pk PRIMARY KEY ( admissionnum,
                                        roomnum,
                                        startdate )
-)
+);
