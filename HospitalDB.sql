665e34c8183067dbe0623e71f3cf2b4fade5de72
km5762 <km5762@DESKTOP-587P2TU>
2023-02-05 17:08:15 -0500

    Tables finished

--- HospitalDB.sql
+++ HospitalDB.sql
@@ -1,15 +1,15 @@
 CREATE TABLE employee (
     employeeid    INTEGER,
-    firstname     VARCHAR2(40),
-    lastname      VARCHAR2(40),
+    firstname     VARCHAR2(40) NOT NULL,
+    lastname      VARCHAR2(40) NOT NULL,
     salary        INTEGER,
     jobtitle      VARCHAR(40),
     officenum     INTEGER,
-    employeerank  VARCHAR2(40),
-    supervisorid  INTEGER,
-    addressstreet VARCHAR2(40),
-    addresscity   VARCHAR2(40),
-    addresszip    INTEGER,
+    employeerank  VARCHAR2(40) NOT NULL,
+    supervisorid  INTEGER NOT NULL,
+    addressstreet VARCHAR2(40) NOT NULL,
+    addresscity   VARCHAR2(40) NOT NULL,
+    addresszip    INTEGER NOT NULL,
     CONSTRAINT employee_supervisorid_fk FOREIGN KEY ( supervisorid )
         REFERENCES employee ( employeeid ),
     CONSTRAINT employee_pk PRIMARY KEY ( employeeid )
@@ -17,9 +17,9 @@ CREATE TABLE employee (
 
 CREATE TABLE doctor (
     employeeid    INTEGER,
-    gender        VARCHAR2(20),
+    gender        VARCHAR2(20) NOT NULL,
     specialty     VARCHAR2(20),
-    graduatedfrom VARCHAR2(20),
+    graduatedfrom VARCHAR2(20) NOT NULL,
     CONSTRAINT doctor_pk PRIMARY KEY ( employeeid ),
     CONSTRAINT doctor_employeeid_fk FOREIGN KEY ( employeeid )
         REFERENCES employee ( employeeid )
@@ -34,7 +34,7 @@ CREATE TABLE equipmenttechnician (
 
 CREATE TABLE room (
     roomnum      INTEGER,
-    occupiedflag CHAR(1),
+    occupiedflag CHAR(1) DEFAULT '0' NOT NULL,
     CONSTRAINT occupied_check CHECK ( occupiedflag IN ( '1', '0' ) ),
     CONSTRAINT room_pk PRIMARY KEY ( roomnum )
 );
@@ -42,7 +42,7 @@ CREATE TABLE room (
 CREATE TABLE equipmenttype (
     equipmenttypeid INTEGER,
     equipmentdesc   VARCHAR2(500),
-    equipmentmodel  VARCHAR2(500),
+    equipmentmodel  VARCHAR2(500) NOT NULL,
     instructions    VARCHAR2(500),
     numberofunits   INTEGER,
     CONSTRAINT equipmenttypeid_pk PRIMARY KEY ( equipmenttypeid )
@@ -91,23 +91,23 @@ CREATE TABLE roomaccess (
     CONSTRAINT roomaccess_pk PRIMARY KEY ( roomnum,
                                            employeeid )
 );
---ssn will be "111-222-3333"
+--ssn will be '111-222-3333'
 CREATE TABLE patient (
     ssn       CHAR(12),
-    firstname VARCHAR2(40),
-    lastname  VARCHAR2(40),
-    address   VARCHAR2(40),
-    telnum    INTEGER,
+    firstname VARCHAR2(40) NOT NULL,
+    lastname  VARCHAR2(40) NOT NULL,
+    address   VARCHAR2(40) NOT NULL,
+    telnum    INTEGER NOT NULL,
     CONSTRAINT pk_patient PRIMARY KEY ( ssn )
 );
 
 CREATE TABLE admission (
     admissionnum     INTEGER,
-    admissiondate    DATE,
+    admissiondate    DATE NOT NULL,
     leavedate        DATE,
     totalpayment     NUMBER,
     insurancepayment NUMBER,
-    ssn              CHAR(12),
+    ssn              CHAR(12) NOT NULL,
     futurevisit      DATE,
     FOREIGN KEY ( ssn )
         REFERENCES patient ( ssn ),
