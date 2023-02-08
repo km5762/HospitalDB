500c8f8470d1c40774f0142493cfdd11eb824e74
km5762 <km5762@DESKTOP-587P2TU>
2023-02-08 15:26:30 -0500

    Finish query 12

--- Queries.sql
+++ Queries.sql
@@ -5,3 +5,7 @@ SELECT * FROM equipmenttype WHERE equipmenttypeid IN (SELECT equipmenttypeid FRO
 SELECT futurevisit FROM admission WHERE admissiondate = (SELECT MAX(admissiondate) FROM admission WHERE ssn = '111-22-3333');
 
 -- NUMBER 11
+SELECT employeeid FROM (SELECT admissionnum FROM admission WHERE ssn = '111-22-3333') r1, examine WHERE r1.admissionnum = examine.admissionnum GROUP BY employeeid HAVING count(employeeid) > 2;
+
+-- NUMBER 12
+(SELECT equipmenttypeid FROM equipment WHERE purchaseyear = 2010) INTERSECT (SELECT equipmenttypeid FROM equipment WHERE purchaseyear = 2011);
