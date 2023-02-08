fe7e3c8c7a0af7c50516d534542499467e69fca3
km5762 <km5762@DESKTOP-587P2TU>
2023-02-08 14:59:36 -0500

    Finish query 9

--- Queries.sql
+++ Queries.sql
@@ -1,1 +1,2 @@
-SELECT * FROM PATIENT;
+-- NUMBER 9
+SELECT * FROM equipmenttype WHERE equipmenttypeid IN (SELECT equipmenttypeid FROM canrepairequipment GROUP BY equipmenttypeid HAVING COUNT(employeeid) < 2);
