337ff6b033f98a8a667d2178641285a6c9142746
Sargeanthost <sargeanthost@gmail.com>
2023-02-09 17:54:09 -0500

    1-4 done

--- Queries.sql
+++ Queries.sql
@@ -1,11 +1,127 @@
+--NUMBER 1
+SELECT
+    employeeid,
+    specialty,
+    gender,
+    graduatedfrom
+FROM
+    doctor
+WHERE
+    graduatedfrom = 'WPI';
+--NUMBER 2
+SELECT
+    employeeid,
+    firstname
+    || ' '
+    || lastname AS name,
+    salary
+FROM
+    employee
+WHERE
+    supervisorid = 3;
+--NUMBER 3
+SELECT
+    ssn,
+    SUM(insurancepayment) AS totalinsurancepayment
+FROM
+    admission
+GROUP BY
+    ssn;
+--NUMBER 4
+SELECT
+    ssn,
+    firstname,
+    lastname,
+    0 AS numvisits
+FROM
+    patient
+WHERE
+    NOT EXISTS (
+        SELECT
+            1
+        FROM
+            admission
+        WHERE
+            admission.ssn = patient.ssn
+    )
+UNION
+SELECT
+    admission.ssn,
+    firstname,
+    lastname,
+    COUNT(admission.ssn) AS numvisits
+FROM
+    admission, patient
+where admission.ssn = patient.ssn
+GROUP BY
+    admission.ssn, firstname, lastname;
 -- NUMBER 9
-SELECT * FROM equipmenttype WHERE equipmenttypeid IN (SELECT equipmenttypeid FROM canrepairequipment GROUP BY equipmenttypeid HAVING COUNT(employeeid) < 2);
+SELECT
+    *
+FROM
+    equipmenttype
+WHERE
+    equipmenttypeid IN (
+        SELECT
+            equipmenttypeid
+        FROM
+            canrepairequipment
+        GROUP BY
+            equipmenttypeid
+        HAVING
+            COUNT(employeeid) < 2
+    );
 
 -- NUMBER 10
-SELECT futurevisit FROM admission WHERE admissiondate = (SELECT MAX(admissiondate) FROM admission WHERE ssn = '111-22-3333');
+SELECT
+    futurevisit
+FROM
+    admission
+WHERE
+    admissiondate = (
+        SELECT
+            MAX(admissiondate)
+        FROM
+            admission
+        WHERE
+            ssn = '111-22-3333'
+    );
 
 -- NUMBER 11
-SELECT employeeid FROM (SELECT admissionnum FROM admission WHERE ssn = '111-22-3333') r1, examine WHERE r1.admissionnum = examine.admissionnum GROUP BY employeeid HAVING count(employeeid) > 2;
+SELECT
+    employeeid
+FROM
+    (
+        SELECT
+            admissionnum
+        FROM
+            admission
+        WHERE
+            ssn = '111-22-3333'
+    ) r1,
+    examine
+WHERE
+    r1.admissionnum = examine.admissionnum
+GROUP BY
+    employeeid
+HAVING
+    COUNT(employeeid) > 2;
 
 -- NUMBER 12
-(SELECT equipmenttypeid FROM equipment WHERE purchaseyear = 2010) INTERSECT (SELECT equipmenttypeid FROM equipment WHERE purchaseyear = 2011);
+(
+    SELECT
+        equipmenttypeid
+    FROM
+        equipment
+    WHERE
+        purchaseyear = 2010
+)
+INTERSECT
+(
+    SELECT
+        equipmenttypeid
+    FROM
+        equipment
+    WHERE
+        purchaseyear = 2011
+);
