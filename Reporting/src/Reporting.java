00a908a2984c7e62dd9a0de98ee27af3b5bda3d2
km5762 <km5762@DESKTOP-587P2TU>
2023-02-24 13:41:50 -0500

    Add handling for 3rd arg

--- Reporting/src/Reporting.java
+++ Reporting/src/Reporting.java
@@ -3,9 +3,22 @@ public class Reporting {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         final String username = args[0];
         final String password = args[1];
+        final String option = (args.length > 2) ? args[2] : "none";
 
+        // Making connection
         Class.forName("oracle.jdbc.driver.OracleDriver");
         Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@csorcl.cs.wpi.edu:1521:orcl", username, password);
-        System.out.println(conn);
+        
+        // Reading option
+        switch (option) {
+            case "1":
+                System.out.println("1");
+                break;
+            default:
+                System.out.println("1-Report Patients Basic Information\n" +
+                        "2-Report Doctors Basic Information\n" +
+                        "3-Report Admissions Information\n" +
+                        "4-Update Admissions Payment\n");
+        }
     }
 }
