1049c6487084b8b444b87329d4d8faf3edaae0bc
km5762 <km5762@DESKTOP-587P2TU>
2023-02-24 13:24:02 -0500

    Connected successfully from JDBC to Oracle

--- Reporting/src/Reporting.java
+++ Reporting/src/Reporting.java
@@ -1,4 +1,11 @@
+import java.sql.*;
 public class Reporting {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        final String username = args[0];
+        final String password = args[1];
+
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@csorcl.cs.wpi.edu:1521:orcl", username, password);
+        System.out.println(conn);
     }
 }
