791ab893f88ec03bf675754d863c672683431197
km5762 <km5762@DESKTOP-587P2TU>
2023-02-24 14:14:26 -0500

    Finish rough version of option 1

--- Reporting/src/Reporting.java
+++ Reporting/src/Reporting.java
@@ -1,6 +1,9 @@
 import java.sql.*;
+import java.util.Scanner;
+
 public class Reporting {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        final Scanner scanner = new Scanner(System.in);
         final String username = args[0];
         final String password = args[1];
         final String option = (args.length > 2) ? args[2] : "none";
@@ -8,11 +11,31 @@ public class Reporting {
         // Making connection
         Class.forName("oracle.jdbc.driver.OracleDriver");
         Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@csorcl.cs.wpi.edu:1521:orcl", username, password);
-        
+        Statement stmt = conn.createStatement();
+
         // Reading option
         switch (option) {
             case "1":
-                System.out.println("1");
+                System.out.println("Enter Patient SSN:");
+                final String findSSN = scanner.next();
+                final String query = "SELECT * FROM patient WHERE patient.ssn = " + "\'" + findSSN + "\'";
+                final ResultSet result = stmt.executeQuery(query);
+                String patientSSN = "";
+                String firstName = "";
+                String lastName = "";
+                String address = "";
+
+                while (result.next()) {
+                    patientSSN = result.getString("ssn");
+                    firstName = result.getString("firstname");
+                    lastName = result.getString("lastName");
+                    address = result.getString("address");
+                }
+
+                System.out.println("Patient SSN: " + patientSSN);
+                System.out.println("Patient First Name: " + firstName);
+                System.out.println("Patient Last Name: " + lastName);
+                System.out.println("Patient Address: " + address);
                 break;
             default:
                 System.out.println("1-Report Patients Basic Information\n" +
@@ -20,5 +43,7 @@ public class Reporting {
                         "3-Report Admissions Information\n" +
                         "4-Update Admissions Payment\n");
         }
+        stmt.close();
+        conn.close();
     }
 }
